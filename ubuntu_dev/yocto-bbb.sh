10ebdba1245edac5aefd58bad305b2a1290c1d7b
hsienchieh <linhsienchieh@gmail.com>
2015-05-15 20:02:09 +0800

    Update yocto-bbb.sh

--- ubuntu_dev/yocto-bbb.sh
+++ ubuntu_dev/yocto-bbb.sh
@@ -3,12 +3,10 @@
 
 wget http://free-electrons.com/doc/training/yocto/yocto-labs.tar.xz
 tar xf yocto-labs.tar.xz
-wget http://downloads.yoctoproject.org/releases/yocto/yocto-1.5.1/\
-poky-dora-10.0.1.tar.bz2
-tar xf poky-dora-10.0.1.tar.bz2
 
 wget http://downloads.yoctoproject.org/releases/yocto/yocto-1.5.1/poky-dora-10.0.1.tar.bz2
 tar xf poky-dora-10.0.1.tar.bz2
+
 cd poky-dora-10.0.1
 git clone git://git.yoctoproject.org/meta-ti.git
 cd meta-ti
