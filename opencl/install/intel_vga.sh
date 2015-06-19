31d3016141e918a3b9677f5e1563dd94e2a9b8bb
HsienChieh Lin <linhsienchieh@gmail.com>
2015-06-20 02:38:30 +0800

    install intel driver

---
+++ opencl/install/intel_vga.sh
@@ -0,0 +1,4 @@
+wget https://download.01.org/gfx/ubuntu/14.10/main/pool/main/i/intel-linux-graphics-installer/intel-linux-graphics-installer_1.1.0-0intel1_amd64.deb
+sudo apt-get install ttf-ancient-fonts
+sudo dpkg -i intel-linux-graphics-installer_1.1.0-0intel1_amd64.deb
+rm -f intel-linux-graphics-installer_1.1.0-0intel1_amd64.deb
