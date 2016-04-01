3508c46fac25d55259622f97877c4601d686e74b
HsienChieh Lin <linhsienchieh@gmail.com>
2016-04-01 20:36:40 +0800

    add openbmc build script

---
+++ ubuntu_dev/opbmc.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/openbmc/openbmc.git
+udo apt-get install -y git build-essential libsdl1.2-dev texinfo gawk chrpath diffstat
+.  openbmc/openbmc-env
+bitbake obmc-phosphor-image
+
