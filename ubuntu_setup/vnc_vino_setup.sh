4808bce5213f4f412abc27c7d0d09beafb38c0d8
HsienChieh Lin <linhsienchieh@gmail.com>
2016-04-04 22:20:38 +0800

    add vnc vino setup script

---
+++ ubuntu_setup/vnc_vino_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#vino-preferences 
+gsettings set org.gnmoe.Vino require-encryption false
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino enabled true
+
+
