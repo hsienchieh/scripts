bdff9dedbecd6dd19deff7a7cbb643ccc00e5c99
Hsien-Chieh Lin <linhsienchieh@gmail.com>
2016-08-07 01:25:34 +0800

    Add nodejs and numpy for build bmc

--- ubuntu_dev/bmc_build_env.sh
+++ ubuntu_dev/bmc_build_env.sh
@@ -2,4 +2,5 @@
 # install  bmc build tools
 sudo apt-get install gcc-multilib  mtd-utils:i386 subversion patch patchutils  bison libc6-dev libxml-dom-perl zlib1g zlib1g-dev  libcurl4-openssl-dev libncurses5:i386 python-numpy doxygen  cppcheck  npm  nodejs  php5-cli libpcre3-dev npm python-pip
 sudo npm install grunt  grunt-cli bower coveralls beautifier
-pip install numpy
+sudo pip install numpy
+sudo ln -s /usr/bin/nodejs /usr/bin/node
