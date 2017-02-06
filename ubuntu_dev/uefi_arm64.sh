16088e82ff01215fd73233c10745a159debd2899
hsienchieh <linhsienchieh@gmail.com>
2017-02-06 09:38:53 +0800

    Update uefi_arm64.sh
    Enable Backspace  -D TTY_TERMINAL in build scripts

--- ubuntu_dev/uefi_arm64.sh
+++ ubuntu_dev/uefi_arm64.sh
@@ -13,6 +13,10 @@ wget https://releases.linaro.org/archive/13.07/components/toolchain/binaries/gcc
 
 tar xf gcc-linaro-aarch64-linux-gnu-4.8-2013.07-1_linux.tar.xz
 
+#wget https://releases.linaro.org/components/toolchain/binaries/4.9-2016.02/aarch64-linux-gnu/gcc-linaro-4.9-2016.02-x86_64_aarch64-linux-gnu.tar.xz
+#tar xvf gcc-linaro-4.9-2016.02-x86_64_aarch64-linux-gnu.tar.xz
+
+
 TOOL_PATH=`pwd`
 
 cd edk2
@@ -29,7 +33,9 @@ cp ./BaseTools/Conf/tools_def.template Conf/tools_def.txt
 cp ./BaseTools/Conf/build_rule.template Conf/build_rule.txt
 
 
-GCC48_AARCH64_PREFIX=$TOOL_PATH/gcc-linaro-aarch64-linux-gnu-4.8-2013.07-1_linux/bin/aarch64-linux-gnu- build -a AARCH64 -t GCC48 -p ArmVirtPkg/ArmVirtQemu.dsc
+GCC48_AARCH64_PREFIX=$TOOL_PATH/gcc-linaro-aarch64-linux-gnu-4.8-2013.07-1_linux/bin/aarch64-linux-gnu- build -a AARCH64 -t GCC48 -p ArmVirtPkg/ArmVirtQemu.dsc  -D TTY_TERMINAL      
+
+
 
 popd
 
