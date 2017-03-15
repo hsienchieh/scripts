826f01f5fcabcafbc2d061d04afb038cde85c5fe
Hsien-Chieh Lin <linhsienchieh@gmail.com>
2017-03-15 08:37:18 +0800

    openCV installation script

---
+++ ubuntu_dev/openCv.sh
@@ -0,0 +1,9 @@
+
+
+sudo apt-get install -y build-essential cmake git pkg-config
+sudo apt-get install -y libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
+sudo apt-get install -y libgtk2.0-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y libopencv-dev python-opencv
+
