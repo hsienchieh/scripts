88262d8fb940215eccb93dec1bebb093d74e70b9
Hsien-Chieh <linhsienchieh@gmail.com>
2018-07-10 23:11:37 +0800

    update vino setting

--- ubuntu_setup/vnc_vino_setup.sh
+++ ubuntu_setup/vnc_vino_setup.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-#vino-preferences 
-gsettings set org.gnmoe.Vino require-encryption false
-gsettings set org.gnome.Vino prompt-enabled false
-gsettings set org.gnome.Vino enabled true
-
-
+gsettings set org.gnome.Vino require-encryption false
